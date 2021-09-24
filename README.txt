270b4490359648fc69641335a2f3d6f802c7b759
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-09-24 18:22:17 -0300

    First commit, project complete

---
+++ README.txt
@@ -0,0 +1,16 @@
+BOT ALERT - Telegram - Python
+We create queries to SQL Server and if the records have exceeded the parameters, an alert is created through telegram to attend to the case.
+
+Creamos consultas a SQL Server y si los registros llevan superando los parametros, se crea una alerta atravez de telegram para atender el caso.
+
+#Create VM
+-virtualenv -p python3 env
+
+#Activate
+-env\Scripts>activate.bat
+
+#Driver sqlServer
+-pip install pyodbc
+
+#Conexion a Api
+-pip install requests
---
+++ src/monitoring.py
@@ -0,0 +1,55 @@
+import requests 
+import pyodbc
+
+try:
+    #Conexion BBDD
+    db = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=AGREGARIP;DATABASE=FraudeControl;UID=sa;PWD=Pass.001')
+
+    lista = []
+    #Connector
+    cursor = db.cursor()
+
+    #Querys
+    cursor.execute("SELECT count(idTransaccion) FROM [FraudControl].[dbo].[TransactionWallet] where contado = 0")
+    saldo_noprocess = cursor.fetchval()
+    lista.append(saldo_noprocess)  # Prueba
+    print(saldo_noprocess)
+
+    cursor.execute("SELECT count(IdTxGateway) FROM [FraudControl].[dbo].[Transaction] where TransactionStatusId = 2")
+    transf_noprocess = cursor.fetchval()
+    lista.append(transf_noprocess)
+    print(transf_noprocess)
+
+    cursor.execute("SELECT count (Id)   FROM [FraudControl].[dbo].[OnboardingApiCodes] where StatusApiHttp = 400 and actiondate  >=  DATEADD(MINUTE, -10, GETDATE())")
+    onboard_fail = cursor.fetchval()
+    lista.append(onboard_fail)
+    print(onboard_fail)
+
+    cursor.execute("SELECT count (Id) FROM [FraudControl].[dbo].[OnboardingApiCodes] where StatusApiHttp = 200 and actiondate  >=  DATEADD(MINUTE, -10, GETDATE())")
+    onboard_success = cursor.fetchval()
+    lista.append(onboard_success)
+    print(onboard_success)
+
+    print(lista)
+    for i in lista:
+        if i > 100:
+            print(i)
+            send = "Alerta en Fraude"
+            id = "@PruebasBotvlady"
+
+            token = "2016605395:AAGkZJl-EjUygap-Dc5QmfG9-clzejMymiM"
+            url = "https://api.telegram.org/bot" + token + "/sendMessage"
+            data = {
+                'chat_id': id,
+                'text': send
+            }
+
+            requests.post(url, params=data)
+            
+except Exception as ex:
+    print(ex)
+
+finally:
+    db.close()
+    print("Monitoreo finalizado.")
